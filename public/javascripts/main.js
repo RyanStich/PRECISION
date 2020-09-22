32b9470ce87ea9ae682a638377d814cea826f738
jack <jacksonmayhew9@gmail.com>
2020-09-21 18:30:51 -0700

    thatll do

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -5,3 +5,5 @@ if ( window.history.replaceState ) {
     window.history.replaceState( null, null, window.location.href );
   }
 
+
+history.replaceState({}, document.title, window.location.href.split('#')[0]);
