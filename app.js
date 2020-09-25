bd1ebd47fcfef61779b142a7837644d5f0a4996b
jack <jacksonmayhew9@gmail.com>
2020-09-24 22:22:10 -0700

    lets deploy

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//  app.use(helmet()); 
+// app.use(helmet()); 
 app.use(compression());
 
 // serve static files
